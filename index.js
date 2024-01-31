25931dcad0bf7eb97233c60a0ab21b52a4977dd8
Joy <github.joysaha@gmail.com>
2024-01-31 21:27:51 +0600

    coding commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const friends = ['navin', 'rabin', 'dabin', 'kabin', 'favin'];
+
+for(const friend of friends){
+    // console.log(friend);
+}
+
+for(let i = 0; i < friends.length; i++){
+    // console.log(i);
+    // console.log(friends[i]);
+}
+
+let i = 0;
+while (i < friends.length){
+    console.log(friends[i]);
+    i++;
+}
