0f8b2fae2a93ba97b18494f3e6c90075f3f211e0
Joy <github.joysaha@gmail.com>
2024-02-01 10:39:50 +0600

    loop coding commit

--- loop.js
+++ loop.js
@@ -8,12 +8,30 @@ for(const num in numbers){
 const fruits = ['orange', 'apple', 'banana', 'watermeloon'];
 
 for(const food of fruits){
-    console.log(food);
-    console.log('i want to eat');
+    // console.log(food);
+    // console.log('i want to eat');
 }
 
 const number = 7;
 if (number < 10){
-    console.log('choto ekta paisi');
+    // console.log('choto ekta paisi');
 }
-console.log('somthing new is cooking');
+// console.log('somthing new is cooking');
+
+// let num = 0;
+// while(num < 5){
+//     console.log('looping');
+//     num = num +1;
+// }
+
+// let num = 1;
+// while (num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+let num = 50;
+while (num <= 100){
+    console.log(num);
+    num++;
+}
