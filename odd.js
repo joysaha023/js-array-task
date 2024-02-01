0aa057b08100139d2dbbe7306bc6bee4ac438759
Joy <github.joysaha@gmail.com>
2024-02-01 11:52:23 +0600

    for and while loop commit

---
+++ odd.js
@@ -0,0 +1,56 @@
+// let i = 61;
+// while(i <= 100){
+//     // console.log(i);
+//     i = i + 2;
+// }
+
+// let i = 78;
+
+// while(i <= 98){
+//     console.log(i);
+//     i = i + 2;
+// }
+
+// let i = 81;
+// let sum = 0;
+// while(i <= 131){
+//     console.log(i)
+//     sum = sum + i;
+//     console.log("odd", sum);
+//     i = i + 2;
+// }
+
+// let number = 81;
+// let sum = 0;
+
+// while (number <= 131) {
+//     if (number % 2 !== 0) {
+//         sum += number;
+//     }
+//     number++;
+// }
+
+// console.log(`The sum of all odd numbers from 81 to 131 is: ${sum}`);
+
+
+// let i = 206;
+// let sum = 0;
+
+// while(i <= 311){
+//     console.log(i)
+//     sum = sum + i;
+//     console.log(sum);
+//     i = i + 2;
+// }
+
+let number = 206;
+let sum = 0;
+
+while (number <= 311) {
+    if (number % 2 === 0) {
+        sum += number;
+    }
+    number++;
+}
+
+console.log(`The sum of all even numbers from 206 to 311 is: ${sum}`);
---
+++ sum.js
@@ -0,0 +1,51 @@
+// let num = 1;
+// let sum = 0;
+// while(num <= 10){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log('sum :', sum);
+//     num++;
+// }
+
+// let num = 1;
+// while (num <= 10){
+//     console.log(num);
+
+//     if(num % 2 === 0){
+//         console.log('even number', num);
+//     }
+//     num++;
+// }
+// // console.log(89);
+
+// let texti = "I will invest at least 6 hrs every single day for next 60 days!";
+
+// while(texti <= 60){
+//     console.log(texti);
+// }
+
+let days = 1;
+
+while(days <= 60){
+    // console.log("I will invest at least 6 hrs every single day for next 60 days!");
+    days++;
+}
+
+// for(i = 1; i <= 60; i++){
+//     // console.log("I will invest at least 6 hrs every single day for next 60 days!");
+//     i++;
+// }
+
+// // for(i = 0; i <= 10; i++){
+//     // console.log(i);
+//     i++;
+// }
+
+// // for(i=61; i <= 100; i = i+2){
+//     // console.log(i);
+    
+// }
+
+for(i = 78; i <= 98; i = i + 2){
+    console.log(i);
+}
