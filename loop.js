cf3323ec4c7a6dc33506b157a5c8e94915cb61f1
Joy <github.joysaha@gmail.com>
2024-02-01 10:08:54 +0600

    coding commit first

---
+++ loop.js
@@ -0,0 +1,19 @@
+const numbers = [34, 55, 67,99,90,32,23];
+// console.log(numbers)
+
+for(const num in numbers){
+    // console.log(num);
+}
+
+const fruits = ['orange', 'apple', 'banana', 'watermeloon'];
+
+for(const food of fruits){
+    console.log(food);
+    console.log('i want to eat');
+}
+
+const number = 7;
+if (number < 10){
+    console.log('choto ekta paisi');
+}
+console.log('somthing new is cooking');
